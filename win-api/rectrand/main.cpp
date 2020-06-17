8623af770162cc54e2a977b7c9b6ec21a64a74ae
Shahimian <shahimian@gmail.com>
2020-06-17 11:46:45 +0430

    complete rectangle random

--- win-api/rectrand/main.cpp
+++ win-api/rectrand/main.cpp
@@ -1,8 +1,18 @@
 #include <windows.h>
+#include <stdlib.h>
+
+void DrawRectangle(HWND);
+
+int cxClient, cyClient;
 
 /* This is where all the input to the window goes to */
 LRESULT CALLBACK WndProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam) {
 	switch(Message) {
+		
+		case WM_SIZE:
+			cxClient = LOWORD(lParam);
+			cyClient = HIWORD(lParam);
+			return 0;
 		
 		/* Upon destruction, tell the main thread to stop */
 		case WM_DESTROY: {
@@ -59,9 +69,32 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		sent to WndProc. Note that GetMessage blocks code flow until it receives something, so
 		this loop will not produce unreasonably high CPU usage
 	*/
-	while(GetMessage(&msg, NULL, 0, 0) > 0) { /* If no error is received... */
-		TranslateMessage(&msg); /* Translate key codes to chars if present */
-		DispatchMessage(&msg); /* Send it to WndProc */
+	while(TRUE) {
+		if(PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)){
+			if(msg.message == WM_QUIT)
+				break;
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		} else {
+			DrawRectangle(hwnd);
+		}
 	}
 	return msg.wParam;
 }
+
+void DrawRectangle(HWND hwnd){
+	HBRUSH hBrush;
+	HDC hdc;
+	RECT rect;
+	if(cxClient == 0 || cyClient == 0)
+		return;
+	SetRect(&rect, rand() % cxClient, rand() % cyClient,
+		rand() % cxClient, rand() % cyClient);
+	hBrush = CreateSolidBrush(RGB(rand() % 256, rand() % 256, rand() % 256));
+	hdc = GetDC(hwnd);
+	FillRect(hdc, &rect, hBrush);
+	ReleaseDC(hwnd, hdc);
+	DeleteObject(hBrush);
+	return;
+}
+
